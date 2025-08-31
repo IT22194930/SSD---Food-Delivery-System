02ff8e42587ec9aefda459093fd6edfe9811066d
Nadil <liyanage.nadil@gmail.com>
2025-08-31 15:11:39 +0530

    A05:2021 – Security Misconfiguration: Fixed
    A05:2021 – Security Misconfiguration: Fixed

--- backend/restaurant-service/server.js
+++ backend/restaurant-service/server.js
@@ -11,7 +11,25 @@ const app = express();
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+// Vulnerability 1 - CORS configuration
+const allowedOrigins = [process.env.CLIENT_URL || "http://localhost:3030"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // allow cookies/auth headers if needed
+  })
+);
+//-----------------------------------------------//
 
 app.use("/api/restaurants", restaurantRoutes);
 app.use("/api/menu", menuRoutes);
